b6fce253210931db33d0aa8c46aae43929d5d9e5
JwOhEight <garbageday0810@gmail.com>
2024-02-17 00:24:07 -0600

    Finish playRound function

--- main.js
+++ main.js
@@ -9,9 +9,19 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection = computerSelection) {
+        console.log("It's a tie!")
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Rock")
+    ) {
+        console.log("You win! " + playerSelection + " beats " + computerSelection + "!");
+    } else {
+        console.log("You Lose! " + computerSelection + " beats " + playerSelection + "!");
+    }
 }
   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
