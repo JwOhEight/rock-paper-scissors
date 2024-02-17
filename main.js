e00a62744be6fd1bb6a988743e881ec8d993f1f3
JwOhEight <garbageday0810@gmail.com>
2024-02-17 01:09:57 -0600

    Add win and lose conditions

--- main.js
+++ main.js
@@ -7,24 +7,55 @@ const choiceOptions = [
 function getComputerChoice() {
     return choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
 }
+
 function playGame() {
     function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
-    } else if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Rock")
-    ) {
-        console.log("You win! " + playerSelection + " beats " + computerSelection + "!");
-    } else {
-        console.log("You Lose! " + computerSelection + " beats " + playerSelection + "!");
+        if (playerSelection === computerSelection) {
+            return "It's a tie!";
+        } else if (
+            (playerSelection === "Rock" && computerSelection === "Scissors") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper") ||
+            (playerSelection === "Paper" && computerSelection === "Rock")
+        ) {
+            return "You win! " + playerSelection + " beats " + computerSelection + "!";
+        } else {
+            return "You Lose! " + computerSelection + " beats " + playerSelection + "!";
+        }
     }
-    }  
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let round = 1; round <= 5; round++) {
+        let playerSelection = prompt("Round " + round + ": Rock, Paper, or Scissors?");
+        
+        if (!playerSelection) {
+            console.log("You cancelled the game.");
+            return;
+        }
+
+        console.log("You chose " + playerSelection + ".");
+        let computerSelection = getComputerChoice();
+        console.log("The computer chose " + computerSelection + ".");
+
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You won the game!");
+    } else if (playerScore < computerScore) {
+        console.log("Sorry! You lost the game.");
+    } else {
+        console.log("The game is a tie!");
+    }
 }
 
-let playerSelection = prompt("Rock, Paper, or Scissors?")
-console.log("You chose " + playerSelection + ".");
-let computerSelection = getComputerChoice();
-console.log("The computer chose " + computerSelection + ".");
-playRound(playerSelection, computerSelection);
+// Start the game
+playGame();
